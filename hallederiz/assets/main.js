144cedbc771083c60707549dd77984016c92cba5
Cennet uçak <ucakcennet83@gmail.com>
2026-05-17 20:13:37 +0300

    wrong uplaod sorry..

--- hallederiz/assets/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-
-    const glow = document.querySelector(".mouse-glow");
-
-    glow.style.left = e.clientX + "px";
-    glow.style.top = e.clientY + "px";
-
-});
